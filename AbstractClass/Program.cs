d8ccee1d6d8c5fd289e53156471dc720c30424ca
myasinK <yasinul@gmail.com>
2018-08-16 16:18:12 -0700

    Finish drill on abstract classes

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public abstract class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public abstract void SayName();
+    }
+
+    public class Employee : Person
+    {
+        public int Id = 0;
+        public override void SayName()
+        {
+            Console.WriteLine("Employee Name: " + FirstName + " " + LastName);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+        }
+    }
+}
